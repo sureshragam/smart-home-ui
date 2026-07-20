e113e89e8e967675adeba28dd9c72f0b32b4dc5b
suresh <suresh.ragam@icloud.com>
2026-07-20 17:09:55 +0530

    feat: add ANDROID_CAMERA type to DeviceType

--- src/types/api/device.ts
+++ src/types/api/device.ts
@@ -4,7 +4,8 @@ export type DeviceType =
 	| "ARDUINO_UNO"
 	| "ARDUINO_NANO"
 	| "RASPBERRY_PI"
-	| "SMART_SWITCH";
+	| "SMART_SWITCH"
+	| "ANDROID_CAMERA";
 
 export type DeviceStatus = "ONLINE" | "OFFLINE";
 
